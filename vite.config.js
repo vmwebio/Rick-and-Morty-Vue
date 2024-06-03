cee12d1cad3ff5fd4ade9d7b20b15a6285a51e5a
vmwebio <vmwebio@gmail.com>
2024-06-03 16:40:55 +0300

    feat: add github pages settings

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Rick-and-Morty-Vue/', // имя репозитория
   plugins: [
     vue(),
   ],
